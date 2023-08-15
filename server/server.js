803effe5d9a2f8b7d66334f3411591aff46911df
pbullock08 <126013657+pbullock08@users.noreply.github.com>
2023-08-15 17:39:46 -0400

    added necessary packages for apollo server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 // implement Apollo Server
 const express = require('express');
 const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express')
+const { expressMiddleware } = require('@apollo/server/express4')
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 
@@ -19,16 +19,20 @@ const server = new ApolloServer({
 const startApolloServer = async () => {
   await server.start();
 
-  app.use(express.urlencoded({ extended: false }));
+  app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
   app.use('graphql', expressMiddleware(server, {
     context: authMiddleware
   }));
 
-  // if we're in production, serve client/build as static assets
+  // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    })
   }
 
   // app.use(routes);
